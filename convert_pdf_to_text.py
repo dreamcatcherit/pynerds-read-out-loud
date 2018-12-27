e3d6921a64577ee2b07b6ffcdc607bb17ce590eb
Ashraful Dowla <44186340+Ashraful-Dowla@users.noreply.github.com>
2018-12-27 23:29:00 +0600

    Updating the file convert_pdf_to_text.py

--- convert_pdf_to_text.py
+++ convert_pdf_to_text.py
@@ -1,21 +1,25 @@
 import PyPDF2
 import textract
 
-filename="read_file.pdf" 
-pdfFileObj=open(filename,'rb')
-pdfReader=PyPDF2.PdfFileReader(pdfFileObj)
-num_pages=pdfReader.numPages
-count=0
-tex=""
+def convert_pdf_to_text(filename):
+    filename+='.pdf' 
+    pdfFileObj=open(filename,'rb')
+    pdfReader=PyPDF2.PdfFileReader(pdfFileObj)
+    num_pages=pdfReader.numPages
+    count=0
+    tex=""
 
-while count<num_pages:
-    pageObj=pdfReader.getPage(count)
-    count+=1
-    tex+=pageObj.extractText()
+    while count<num_pages:
+        pageObj=pdfReader.getPage(count)
+        count+=1
+        tex+=pageObj.extractText()
 
-li=list()
-li=tex.splitlines()
-tex=""
-for t in li:
-    tex+=(" "+t)
-print(tex)
+    li=list()
+    li=tex.splitlines()
+    tex=""
+    for t in li:
+        tex+=(" "+t)
+    return tex
+
+st=input()
+print(convert_pdf_to_text(st))
