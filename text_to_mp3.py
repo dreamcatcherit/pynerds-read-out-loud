d262fb2601a7898d53a2b355bc3d153a946088e2
Tanjil <taaanjil@gmail.com>
2018-12-30 02:03:38 +0600

    i have updated my code using (with) keyword

--- text_to_mp3.py
+++ text_to_mp3.py
@@ -1,11 +1,5 @@
-import pyttsx3
 from gtts import gTTS
-
-engine = pyttsx3.init(driverName='sapi5')
-infile = "tanjil.txt"
-f = open(infile, 'r')
-theText = f.read()
-f.close()
-tts = gTTS(text=theText, lang='en')
-tts.save("saved_file.mp3")
-print("File saved!")
+with open("tanjil.txt",'r') as infile:
+    tts = gTTS(text=infile.read(), lang='en')
+    tts.save("saved_file.mp3")
+    print("File saved!")
