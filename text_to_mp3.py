efaf674425ed5ab9180070ad7211239231aa4e19
Taaanjil <taaanjil@gmail.com>
2019-02-01 23:06:04 +0600

    final code with command line

--- text_to_mp3.py
+++ text_to_mp3.py
@@ -1,11 +1,26 @@
-import pyttsx3
+import os
 from gtts import gTTS
-
-engine = pyttsx3.init(driverName='sapi5')
-infile = "tanjil.txt"
-f = open(infile, 'r')
-theText = f.read()
-f.close()
-tts = gTTS(text=theText, lang='en')
-tts.save("saved_file.mp3")
-print("File saved!")
+def text_to_audio(text_file):
+    '''
+    Function:
+      Convert text files into mp3 files
+    
+    Param arg:
+      The text file which is to be converted into mp3
+      
+    Type arg:str
+        
+    Vartype arg:str
+      
+    Returns:
+      This function doesn't return anything
+      
+    '''
+    filename,extension=os.path.splitext(text_file)
+    if (extension=='.txt'):
+        with open(text_file,'r') as f:
+            tts = gTTS(text=f.read(), lang='en')
+            tts.save(filename+".mp3")
+if __name__ == "__main__":
+    text_to_audio("convert_pdf_to_text.txt")
+        
